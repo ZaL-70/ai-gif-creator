e4c5edd010a5d85998b3224733067a6d045538be
psyas23 <psyas23@nottingham.ac.uk>
2025-10-25 15:02:59 +0100

    updated the gif converter

--- services/gif_converter.py
+++ services/gif_converter.py
@@ -1,17 +1,25 @@
-from moviepy.editor import *
+from moviepy import *
+from pathlib import Path
 
 
 def gif_conversion():
+    
+    Path("temp/GIF").mkdir(parents=True, exist_ok=True)
+    output_name = "output.gif"
     # load the file and makes a snippet of it 
     clip = VideoFileClip(r"C:\Users\aryan\projects\coding\ai-gif-creator\temp\Video\thorsten.mp4")
 
     # Gets only the first 3 seconds
-    clip = clip.subclip(0,3)
+    clip = clip.subclipped(0,3)
 
     #save video clip as a gif
     clip.write_gif("gfg_gif.gif")
+    
+        # Construc
+    output_path = Path(output_folder) / output_name
 
-    clip.ipython_display()
-gif_conversion()
+    # Write GIF to disk
+    clip.write_gif(str(output_path))
+
 
 
